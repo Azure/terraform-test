e3e88527296424487c3824ed41112211cad5c9cd
Matt White <matt.ffffff@gmail.com>
2019-07-22 23:28:00 +0100

    Remove -check-variales from linting as not supported in 0.12

--- build/static_utils.rb
+++ build/static_utils.rb
@@ -6,7 +6,7 @@ def lint_tf
   print "INFO: Linting Terraform configurations...\n".yellow  
   
   if ENV['TERRAFORM_VERSION'].start_with?("0.12")
-    message = `terraform validate -check-variables=false >/dev/null`
+    message = `terraform validate >/dev/null`
   elsif ENV['TERRAFORM_VERSION'].start_with?("0.11")
     message = `terraform validate -check-variables=false 2>&1`
   end
