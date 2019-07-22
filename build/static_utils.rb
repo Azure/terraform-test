b4ea35bf1188a1b7dabee8ba70e0e8f5693cc806
Matt White <matt.ffffff@gmail.com>
2019-07-22 04:02:19 +0100

    update Terraform commands for 0.12

--- build/static_utils.rb
+++ build/static_utils.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 def lint_tf
   # Do the linting on current working folder.
   print "INFO: Linting Terraform configurations...\n".yellow  
-  message = `terraform validate -check-variables=false 2>&1`
+  message = `terraform validate 2>&1`
 
   # Check the linting message.
   if not message.empty?
@@ -17,7 +17,7 @@ end
 def style_tf
   # Do the style checking on current working folder.
   print "INFO: Styling Terraform configurations...\n".yellow  
-  message = `terraform fmt -check=true 2>&1`
+  message = `terraform fmt -check 2>&1`
 
   # Check the styling message.
   if not message.empty?
@@ -30,7 +30,7 @@ end
 def format_tf
   # Apply the canonical format and style on current working folder.
   print "INFO: Formatting Terraform configurations...\n".yellow  
-  message = `terraform fmt -diff=true 2>&1`
+  message = `terraform fmt -diff 2>&1`
 
   # Check the styling message.
   if not message.empty?
