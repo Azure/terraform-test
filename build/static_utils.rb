81223494a38ee29d60c50f98f040ebc58168e2cf
Yuping Wei <56525716+yupwei68@users.noreply.github.com>
2020-03-19 10:50:37 +0800

    Support Terraform v0.12 (#16)
    * add terraform init
    
    * fix terraform validate

--- build/static_utils.rb
+++ build/static_utils.rb
@@ -6,7 +6,11 @@ def lint_tf
   print "INFO: Linting Terraform configurations...\n".yellow  
   
   if ENV['TERRAFORM_VERSION'].start_with?("0.12")
-    message = `terraform validate >/dev/null`
+    success = system ("terraform init")
+        if not success
+          raise "ERROR: terraform init failed!\n".red
+        end
+    message = `terraform validate ./ >/dev/null`
   elsif ENV['TERRAFORM_VERSION'].start_with?("0.11")
     message = `terraform validate -check-variables=false 2>&1`
   end
